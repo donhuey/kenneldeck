9026c938f7c190dcd6ee83467c3a27a3724fe56b
Donhuey <35469316+donhuey@users.noreply.github.com>
2023-01-23 00:34:45 -0600

    Update main.js

--- main.js
+++ main.js
@@ -34,9 +34,8 @@ const scene = new THREE.Scene();
             // let loader = THREE.GLTFLoader;
             loader.setDRACOLoader(dracoLoader);
             loader.load("/HustleDeck.gltf", function (gltf) {
-                gltf.scene.scale.set(0.0129, 0.0129, 0.0129); 
+                gltf.scene.scale.set(0.0128, 0.0128, 0.0128); 
                 let deck = gltf.scene;
-                deck.position.set(0.4,0.2,0)
                 scene.add(deck);
     
                 // animate();
